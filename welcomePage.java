fed2effb0d73cd70256bd6f71f421c283bb3821a
prakhar4527 <40262304+prakhar4527@users.noreply.github.com>
2018-06-26 16:50:04 +0530

    Update welcomePage.java

--- welcomePage.java
+++ welcomePage.java
@@ -1,7 +1,11 @@
 package qainfotech.tatocPageObjects;
 
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class welcomePage
 {
@@ -12,11 +16,13 @@ public class welcomePage
 	}
 	public GridGate OnClickBasicCourse()
 	{
-		clickBasic();
-		return new GridGate(driver);
-	}
-	public void clickBasic()
-	{
 		driver.findElement(By.linkText("Basic Course")).click();
+		WebElement e= driver.findElement(By.tagName("title"));
+		/*String expectedUrl = "http://10.0.1.86/tatoc/basic/grid/gate";
+		Assert.assertEquals(expectedUrl, driver.getCurrentUrl(), "Didn't navigate to correct webpage");*/
+		String title = e.getText();
+		assertEquals("Grid Gate - Basic Course - T.A.T.O.C", driver.getTitle());
+
+		return new GridGate(driver);
 	}
 }
